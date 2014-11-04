3336929003b5bbd934ee95c8fe90c2e4670f090c
Tomoaki Nakajima <powered.by.solaris@gmail.com>
2014-11-04 20:22:22 +0900

    first commit

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+yum install -y -q epel-release
+yum install -y -q gcc gcc-c++ make \
+   python-devel python-pip \
+   openssl-devel \
+   libxml2 libxml2-devel \
+   libxslt libxslt-devel \
+   libffi libffi-devel \
+   libyaml libyaml-devel
+pip install -q -r requirements.txt
